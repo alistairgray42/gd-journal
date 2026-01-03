f2e6fdcd07179b1aba7cb5fa2a65b5e136427b03
Alistair Gray <ajgray@stripe.com>
2026-01-03 12:03:40 -0500

    fix

--- book/generate.py
+++ book/generate.py
@@ -400,7 +400,7 @@ def render_html_document(content: str, title: str = "Grateful Dead Setlists",
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Crimson+Text:ital,wght@0,400;0,600;1,400&family=EB+Garamond:ital,wght@0,400;0,600;1,400&family=Playfair+Display:wght@400;700&family=Source+Sans+Pro:wght@400;600&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="templates/style.css">
+  <link rel="stylesheet" href="/templates/style.css">
 </head>
 <body class="layout-{layout}">
 {content}
